a6de17c1eb6dc067ca9a77dcff7e9a4af03ccac7
Javier <fj.gutierrezs91@gmail.com>
2017-03-23 18:55:32 +0100

    Click to get fuenla images from flickr

--- jsonp.js
+++ jsonp.js
@@ -1,6 +1,14 @@
+$(document).ready(function(){
+      $("#fuenla").click(function(){
+            $("head").append('<script type="text/javascript" charset="utf-8" \
+            src="http://api.flickr.com/services/feeds/photos_public.gne?tags=fuenlabrada&tagmode=any&format=json&jsoncallback=f"></script>');
+      });
+});
+
 function f(jsonp) {
       var items = jsonp.items;
-
+      
+      $("#media").html("");
       for (var i = 0 ; i < items.length; i++) {
             $("#media").append('<img src="' + items[i].media.m + '"> </img>');
       }
