ba1e431acd6bd0046c26ef14904af4febe34985f
Javier <fj.gutierrezs91@gmail.com>
2017-03-23 19:29:43 +0100

    Multiple categories list done

--- jsonp.js
+++ jsonp.js
@@ -1,15 +1,39 @@
 $(document).ready(function(){
+
       $("#fuenla").click(function(){
-            $("head").append('<script type="text/javascript" charset="utf-8" \
-            src="http://api.flickr.com/services/feeds/photos_public.gne?tags=fuenlabrada&tagmode=any&format=json&jsoncallback=f"></script>');
+            setScript("fuenlabrada");
+      });
+
+      $("#clear").click(function(){
+            $("#media").html("");
+      });
+
+      $("#category").click(function(){
+            var categories = $("#list").val();
+            categories = categories.split(" ");
+
+            for (var i = 0 ; i < categories.length; i++) {
+                  setScript(categories[i]);
+            }
       });
 });
 
 function f(jsonp) {
       var items = jsonp.items;
-      
-      $("#media").html("");
+
+      $("#media").append("<h2>" + jsonp.title + "</h2>")
       for (var i = 0 ; i < items.length; i++) {
-            $("#media").append('<img src="' + items[i].media.m + '"> </img>');
+            setImage(items[i].media.m);
       }
+}
+
+var setImage = function (src) {
+      $("#media").append('<img src="' + src + '"> </img>');
+}
+
+var setScript = function (category) {
+      $("#media").html("");
+
+      $("head").append('<script type="text/javascript" charset="utf-8" \
+      src="http://api.flickr.com/services/feeds/photos_public.gne?tags=' + category + '&tagmode=any&format=json&jsoncallback=f"></script>');
 }
